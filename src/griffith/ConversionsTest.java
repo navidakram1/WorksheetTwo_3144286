60fab3bafc3e6180b2ad4832ae3c62fcb7a061cc
NavidAkram <navid.youtub@gmail.com>
2025-03-08 21:49:10 +0000

    Added unit test for euroToDollar

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -1,1 +1,15 @@
 package griffith; 
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ConversionsTest {
+
+    @Test
+    public void testEuroToDollar() {
+        Conversions converter = new Conversions();
+        assertEquals(1.18, converter.euroToDollar(1), 0.01); // 1 Euro = 1.18 Dollars
+        assertEquals(11.8, converter.euroToDollar(10), 0.01); // 10 Euro = 11.8 Dollars
+        assertEquals(0.0, converter.euroToDollar(0), 0.01); // 0 Euro = 0 Dollars
+    }
+}
